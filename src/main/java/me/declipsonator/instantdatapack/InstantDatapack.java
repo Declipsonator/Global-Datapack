2727137615b6814ff88ce3ac072eaa73adb4c882
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2022-03-17 07:08:48 -0400

    Add files via upload

---
+++ src/main/java/me/declipsonator/instantdatapack/InstantDatapack.java
@@ -0,0 +1,25 @@
+package me.declipsonator.instantdatapack;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+public class InstantDatapack implements ModInitializer {
+    public static Logger LOG = LogManager.getLogger();
+    public static File globalPackFolder = new File(FabricLoader.getInstance().getGameDir().toString() + "\\datapacks");
+
+
+    @Override
+    public void onInitialize() {
+        try {
+            globalPackFolder.mkdirs();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        LOG.info("Initialized InstantDatapack");
+    }
+}
---
+++ src/main/java/me/declipsonator/instantdatapack/mixins/MinecraftClientMixin.java
@@ -0,0 +1,22 @@
+package me.declipsonator.instantdatapack.mixins;
+
+import me.declipsonator.instantdatapack.InstantDatapack;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.*;
+import net.minecraft.util.WorldSavePath;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Inject(method = "createServerDataManager", at = @At("HEAD"), cancellable = true)
+    private static void addGlobal(LevelStorage.Session session, CallbackInfoReturnable<ResourcePackManager> cir) {
+        cir.setReturnValue(new ResourcePackManager(ResourceType.SERVER_DATA, new VanillaDataPackProvider(),
+                new FileResourcePackProvider(session.getDirectory(WorldSavePath.DATAPACKS).toFile(), ResourcePackSource.PACK_SOURCE_WORLD),
+                new FileResourcePackProvider(InstantDatapack.globalPackFolder, ResourcePackSource.PACK_SOURCE_WORLD)));
+        cir.cancel();
+    }
+}
